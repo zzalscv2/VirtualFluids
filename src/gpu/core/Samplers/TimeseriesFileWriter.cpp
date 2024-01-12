99a5949ead67fb189fdccd421b4a0962baf61c9f
Henry <henry.korb@geo.uu.se>
2024-01-12 15:11:07 +0100

    add License Header

--- src/gpu/core/Samplers/TimeseriesFileWriter.cpp
+++ src/gpu/core/Samplers/TimeseriesFileWriter.cpp
@@ -1,3 +1,34 @@
+//=======================================================================================
+// ____          ____    __    ______     __________   __      __       __        __
+// \    \       |    |  |  |  |   _   \  |___    ___| |  |    |  |     /  \      |  |
+//  \    \      |    |  |  |  |  |_)   |     |  |     |  |    |  |    /    \     |  |
+//   \    \     |    |  |  |  |   _   /      |  |     |  |    |  |   /  /\  \    |  |
+//    \    \    |    |  |  |  |  | \  \      |  |     |   \__/   |  /  ____  \   |  |____
+//     \    \   |    |  |__|  |__|  \__\     |__|      \________/  /__/    \__\  |_______|
+//      \    \  |    |   ________________________________________________________________
+//       \    \ |    |  |  ______________________________________________________________|
+//        \    \|    |  |  |         __          __     __     __     ______      _______
+//         \         |  |  |_____   |  |        |  |   |  |   |  |   |   _  \    /  _____)
+//          \        |  |   _____|  |  |        |  |   |  |   |  |   |  | \  \   \_______
+//           \       |  |  |        |  |_____   |   \_/   |   |  |   |  |_/  /    _____  |
+//            \ _____|  |__|        |________|   \_______/    |__|   |______/    (_______/
+//
+//  This file is part of VirtualFluids. VirtualFluids is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  VirtualFluids is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+//  for more details.
+//
+//  SPDX-License-Identifier: GPL-3.0-or-later
+//  SPDX-FileCopyrightText: Copyright © VirtualFluids Project contributors, see AUTHORS.md in root folder
+//
+//! \addtogroup gpu_PreCollisionInteractor PreCollisionInteractor
+//! \ingroup gpu_core core
+//! \{
 #include "TimeseriesFileWriter.h"
 
 #include <string>
@@ -10,7 +41,7 @@
 
 #include <basics/DataTypes.h>
 
-void TimeseriesFileWriter::writeHeader(const std::string& fileName, int numberOfPoints, std::vector<std::string>& variableNames, const real* coordsX, const real* coordsY, const real* coordsZ) const
+void TimeseriesFileWriter::writeHeader(const std::string& fileName, int numberOfPoints, std::vector<std::string>& variableNames, const real* coordsX, const real* coordsY, const real* coordsZ)
 {
     std::filesystem::create_directories(std::filesystem::path(fileName).parent_path());
     std::ofstream out(fileName.c_str(), std::ios::out | std::ios::binary);
@@ -33,10 +64,12 @@ void TimeseriesFileWriter::writeHeader(const std::string& fileName, int numberOf
     out.close();
 }
 
-void TimeseriesFileWriter::appendData(const std::string& fileName, std::vector<std::vector<real>>& data) const {
+void TimeseriesFileWriter::appendData(const std::string& fileName, std::vector<std::vector<real>>& data) {
     std::ofstream out(fileName.c_str(), std::ios::app | std::ios::binary);
     for (auto& timestepData : data) {
         out.write((char*)timestepData.data(), sizeof(real) * timestepData.size());
     }
     out.close();
 }
+
+//! \}
--- src/gpu/core/Samplers/TimeseriesFileWriter.h
+++ src/gpu/core/Samplers/TimeseriesFileWriter.h
@@ -1,3 +1,34 @@
+//=======================================================================================
+// ____          ____    __    ______     __________   __      __       __        __
+// \    \       |    |  |  |  |   _   \  |___    ___| |  |    |  |     /  \      |  |
+//  \    \      |    |  |  |  |  |_)   |     |  |     |  |    |  |    /    \     |  |
+//   \    \     |    |  |  |  |   _   /      |  |     |  |    |  |   /  /\  \    |  |
+//    \    \    |    |  |  |  |  | \  \      |  |     |   \__/   |  /  ____  \   |  |____
+//     \    \   |    |  |__|  |__|  \__\     |__|      \________/  /__/    \__\  |_______|
+//      \    \  |    |   ________________________________________________________________
+//       \    \ |    |  |  ______________________________________________________________|
+//        \    \|    |  |  |         __          __     __     __     ______      _______
+//         \         |  |  |_____   |  |        |  |   |  |   |  |   |   _  \    /  _____)
+//          \        |  |   _____|  |  |        |  |   |  |   |  |   |  | \  \   \_______
+//           \       |  |  |        |  |_____   |   \_/   |   |  |   |  |_/  /    _____  |
+//            \ _____|  |__|        |________|   \_______/    |__|   |______/    (_______/
+//
+//  This file is part of VirtualFluids. VirtualFluids is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU General Public
+//  License as published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  VirtualFluids is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+//  for more details.
+//
+//  SPDX-License-Identifier: GPL-3.0-or-later
+//  SPDX-FileCopyrightText: Copyright © VirtualFluids Project contributors, see AUTHORS.md in root folder
+//
+//! \addtogroup gpu_Probes Probes
+//! \ingroup gpu_core core
+//! \{
 #ifndef TIMESERIESFILEWRITER_H
 #define TIMESERIESFILEWRITER_H
 #include <string>
@@ -21,8 +52,10 @@ class TimeseriesFileWriter{
 public: 
     TimeseriesFileWriter() = default;
 
-    void writeHeader(const std::string& fileName, int numberOfPoints, std::vector<std::string>& variableNames, const real* coordsX, const real* coordsY, const real* coordsZ) const;
-    void appendData(const std::string& fileName, std::vector<std::vector<real>>& data) const;
+    static void writeHeader(const std::string& fileName, int numberOfPoints, std::vector<std::string>& variableNames, const real* coordsX, const real* coordsY, const real* coordsZ);
+    static void appendData(const std::string& fileName, std::vector<std::vector<real>>& data);
 };
 
 #endif
+
+//! \}
